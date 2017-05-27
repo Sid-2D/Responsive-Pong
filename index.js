d36cebfceb56d8ec022653faf376610b5c1b528f
Sid <sid6327@gmail.com>
2017-05-28 00:14:45 +0530

    Adding gameloop refactor.

--- index.js
+++ index.js
@@ -1,16 +1,58 @@
-function $(id) {
-	return document.getElementById(id)
+var PONG = {
+	pressedKeys: []
 }
 
-window.onload = () => {
-	var paddleB = $('paddleB')
-	var paddleA = $('paddleA')
+var KEYS = {
+	'UP': 38,
+	'S': 83,
+	'W': 87,
+	'DOWN': 40
+}
+
+
+function refactor() {
+	PONG.timer = setInterval(gameLoop, 30)
+	document.addEventListener('keydown', event => {
+		PONG.pressedKeys[event.keyCode] = true
+	})
+	document.addEventListener('keyup', event => {
+		PONG.pressedKeys[event.keyCode] = false
+	})
+}
+
+function gameLoop() {
+	movePaddles()
+}
+
+function movePaddles() {
+	var paddleB = document.getElementById('paddleB')
+	var paddleA = document.getElementById('paddleA')
+	if (PONG.pressedKeys[KEYS.UP]) {
+		var top = getComputedStyle(paddleB)['top']
+		paddleB.style.top = parseInt(top) - 5 + 'px'
+	}
+	if (PONG.pressedKeys[KEYS.DOWN]) {
+		var top = getComputedStyle(paddleB)['top']
+		paddleB.style.top = parseInt(top) + 5 + 'px'
+	}
+	if (PONG.pressedKeys[KEYS.S]) {
+		var top = getComputedStyle(paddleA)['top']
+		paddleA.style.top = parseInt(top) + 5 + 'px'
+	}
+	if (PONG.pressedKeys[KEYS.W]) {
+		var top = getComputedStyle(paddleA)['top']
+		paddleA.style.top = parseInt(top) - 5 + 'px'
+	}
+}
+
+function oldModel() {
+	var paddleB = document.getElementById('paddleB')
+	var paddleA = document.getElementById('paddleA')
 	document.addEventListener('keydown', event => {
 		console.log(event)
 		switch (event.key) {
 			case 'ArrowUp':
 				var top = getComputedStyle(paddleB)['top']
-				console.log(top)
 				paddleB.style.top = parseInt(top) - 5 + 'px'
 				break
 			case 'ArrowDown': 	
@@ -19,7 +61,6 @@ window.onload = () => {
 				break
 			case 'w':
 				var top = getComputedStyle(paddleA)['top']
-				console.log(top)
 				paddleA.style.top = parseInt(top) - 5 + 'px'
 				break
 			case 's': 	
@@ -29,3 +70,5 @@ window.onload = () => {
 		}
 	})
 }
+
+window.onload = refactor
